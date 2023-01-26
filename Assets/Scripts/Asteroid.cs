1e4e9ba0ff24416feb624ae53018c22788e5ee9b
Galaan <galaan.rouzier@e-artsup.net>
2023-01-26 18:41:19 +0100

    Destruction des Astéroïdes quand ils sortent de la zone

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -58,6 +58,10 @@ public class Asteroid : MonoBehaviour
             Destroy(gameObject);
             Instantiate(ColSFX, transform.position, transform.rotation);
         }
+        else if (collision.gameObject.CompareTag("EntitiesBoundary"))
+        {
+            Destroy(gameObject);
+        }
     }
 
     private Asteroid CreateSplit()
